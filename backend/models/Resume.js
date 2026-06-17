67975aea95bc38f2a27175f2487fc076f6502e73
seharchattha226-creator <seharchattha226@gmail.com>
2026-06-17 19:46:15 +0500

    Remove backend folder and netlify.toml for Vercel deployment

--- backend/models/Resume.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const resumeSchema = new mongoose.Schema({
-  url: { type: String, required: true },
-  filename: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Resume', resumeSchema);
